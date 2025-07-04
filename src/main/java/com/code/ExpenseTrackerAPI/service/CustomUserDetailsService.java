803e93170593aa493b9e5b3d6b862822a83ef73e
r9988.2588 <r9988.2588@gmail.com>
2025-07-04 09:24:02 +0000

    added comments

--- src/main/java/com/code/ExpenseTrackerAPI/service/CustomUserDetailsService.java
+++ src/main/java/com/code/ExpenseTrackerAPI/service/CustomUserDetailsService.java
@@ -17,14 +17,18 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
+    // This method loads the user by username
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // This checks if the user exists
         Users user = userRepository.findByUsername(username);
+
+        // This throws an error if the user does not exist
         if (user == null) {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
 
+        // This returns the user details
         return new CustomUserDetails(user);
     }
-
 }
