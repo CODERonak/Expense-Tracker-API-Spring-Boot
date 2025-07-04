f821ccf9073db4e15325e0e65be35d9f0d13e01d
r9988.2588 <r9988.2588@gmail.com>
2025-07-04 09:03:14 +0000

    successfully configured the custom user details service with the authentication manager with custom user details

--- src/main/java/com/code/ExpenseTrackerAPI/config/SecurityConfig.java
+++ src/main/java/com/code/ExpenseTrackerAPI/config/SecurityConfig.java
@@ -2,29 +2,55 @@ package com.code.ExpenseTrackerAPI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.code.ExpenseTrackerAPI.service.CustomUserDetailsService;
 
 // setting up the security config
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    
     // security filter chain for allowing the requests without any authentication
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+        
         // alowing the register, login and the test page without any authentication
         http.authorizeHttpRequests(requests -> requests
-                .requestMatchers("/register", "/login", "/test").permitAll()
-                .anyRequest().authenticated())
-
-                // enabling the basic authentication
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(form -> form.disable())
-                .csrf(csrf -> csrf.disable());
-
+        .requestMatchers("/register", "/login", "/test").permitAll()
+        .anyRequest().authenticated())
+        
+        // enabling the basic authentication
+        .httpBasic(Customizer.withDefaults())
+        .formLogin(form -> form.disable())
+        .authenticationManager(authenticationManager())
+        .csrf(csrf -> csrf.disable());
+        
         return http.build();
+    }
+    private final CustomUserDetailsService userDetailsService;
+    
+        public SecurityConfig(CustomUserDetailsService userDetailsService) {
+            this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/code/ExpenseTrackerAPI/service/CustomUserDetails.java
@@ -0,0 +1,32 @@
+package com.code.ExpenseTrackerAPI.service;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.code.ExpenseTrackerAPI.entity.Users;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Users user;
+
+    public CustomUserDetails(Users user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
