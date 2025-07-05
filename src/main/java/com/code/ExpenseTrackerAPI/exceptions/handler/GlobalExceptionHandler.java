028c61ce928bc84503f60f666a77d6ed8f6875c9
r9988.2588 <r9988.2588@gmail.com>
2025-07-05 10:22:31 +0000

    added few comments

--- src/main/java/com/code/ExpenseTrackerAPI/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/code/ExpenseTrackerAPI/exceptions/handler/GlobalExceptionHandler.java
@@ -12,8 +12,11 @@ import com.code.ExpenseTrackerAPI.exceptions.payload.ErrorResponse;
 // This controller handles the exceptions
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    // This method handles the UsernameAlreadyExistsException
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex) {
+        // This creates a new error response
+        // and returns it
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.CONFLICT.value(),
                 HttpStatus.CONFLICT.getReasonPhrase(),
@@ -21,8 +24,11 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
     }
 
+    // This method handles the EmailAlreadyExistsException
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleEmailAlreadyExists(EmailAlreadyExistsException ex) {
+        // This creates a new error response
+        // and returns it
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.CONFLICT.value(),
                 HttpStatus.CONFLICT.getReasonPhrase(),
